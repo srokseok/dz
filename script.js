7b8c50292f25937a8157a4688d7972aba4a1d489
srokseok <77568822+srokseok@users.noreply.github.com>
2021-01-24 11:02:06 +0500

    qwerty

---
+++ script.js
@@ -0,0 +1,679 @@
+var name = prompt('введите имя');
+var year = +prompt('введите год рождения');
+var sample1 = +prompt('2+2');
+var sample2 = +prompt('2-2');
+var sample3 = +prompt('2*2');
+var sample4 = +prompt('2/2');
+var num = +prompt('введите секунды');
+console.log('я ' + name + ' мне ' + (2021-year) + 'лет');
+console.log('2 + 2 = 4'  + ' (ваш ответ ' + sample1 + ')' );
+console.log('2 - 2 = 0'  + ' (ваш ответ ' + sample2 + ')' );
+console.log('2 * 2 = 4'  + ' (ваш ответ ' + sample3 + ')' );
+console.log('2 / 2 = 1'  + ' (ваш ответ ' + sample4 + ')' );
+console.log((num/60) + ' минут ' + (num/3600) + ' часов ' + (num/86400) + ' суток ');
+
+var age = +prompt('введите возраст');
+switch (age){
+    case 0:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 1:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 2:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 3:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 4:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 5:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 6:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 7:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 8:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 9:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 10:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 11:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 12:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 13:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 14:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 15:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 16:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 17:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    case 18:
+        alert('Вы еще молоды. Вам нужно учиться');
+        break;
+    
+    case 19:
+        alert('Вам нужно работать');
+        break;
+    
+    case 20:
+        alert('Вам нужно работать');
+        break;
+    
+    case 21:
+        alert('Вам нужно работать');
+        break;
+    
+    case 22:
+        alert('Вам нужно работать');
+        break;
+    
+    case 23:
+        alert('Вам нужно работать');
+        break;
+    
+    case 24:
+        alert('Вам нужно работать');
+        break;
+    
+    case 25:
+        alert('Вам нужно работать');
+        break;
+    
+    case 26:
+        alert('Вам нужно работать');
+        break;
+    
+    case 27:
+        alert('Вам нужно работать');
+        break;
+    
+    case 28:
+        alert('Вам нужно работать');
+        break;
+    
+    case 29:
+        alert('Вам нужно работать');
+        break;
+    
+    case 30:
+        alert('Вам нужно работать');
+        break;
+    
+    case 31:
+        alert('Вам нужно работать');
+        break;
+    
+    case 32:
+        alert('Вам нужно работать');
+        break;
+    
+    case 33:
+        alert('Вам нужно работать');
+        break;
+    
+    case 34:
+        alert('Вам нужно работать');
+        break;
+    
+    case 35:
+        alert('Вам нужно работать');
+        break;
+    
+    case 36:
+        alert('Вам нужно работать');
+        break;
+    
+    case 37:
+        alert('Вам нужно работать');
+        break;
+    
+    case 38:
+        alert('Вам нужно работать');
+        break;
+    
+    case 39:
+        alert('Вам нужно работать');
+        break;
+    
+    case 40:
+        alert('Вам нужно работать');
+        break;
+    
+    case 41:
+        alert('Вам нужно работать');
+        break;
+    
+    case 42:
+        alert('Вам нужно работать');
+        break;
+    
+    case 43:
+        alert('Вам нужно работать');
+        break;
+    
+    case 44:
+        alert('Вам нужно работать');
+        break;
+    
+    case 45:
+        alert('Вам нужно работать');
+        break;
+    
+    case 46:
+        alert('Вам нужно работать');
+        break;
+    
+    case 47:
+        alert('Вам нужно работать');
+        break;
+    
+    case 48:
+        alert('Вам нужно работать');
+        break;
+    
+    case 49:
+        alert('Вам нужно работать');
+        break;
+    
+    case 50:
+        alert('Вам нужно работать');
+        break;
+    
+    case 51:
+        alert('Вам скоро на пенсию');
+        break;
+    
+    case 52:
+        alert('Вам скоро на пенсию');
+        break;
+    
+    case 53:
+        alert('Вам скоро на пенсию');
+        break;
+    
+    case 54:
+        alert('Вам скоро на пенсию');
+        break;
+    
+    case 55:
+        alert('Вам скоро на пенсию');
+        break;
+    
+    case 56:
+        alert('Вам скоро на пенсию');
+        break;
+    
+    case 57:
+        alert('Вам скоро на пенсию');
+        break;
+    
+    case 58:
+        alert('Вам скоро на пенсию');
+        break;
+    
+    case 59:
+        alert('Вам скоро на пенсию');
+        break;
+    
+    case 60:
+        alert('Вы пенсионер');
+        break;
+    
+    case 61:
+        alert('Вы пенсионер');
+        break;
+    
+    case 62:
+        alert('Вы пенсионер');
+        break;
+    
+    case 63:
+        alert('Вы пенсионер');
+        break;
+    
+    case 64:
+        alert('Вы пенсионер');
+        break;
+    
+    case 65:
+        alert('Вы пенсионер');
+        break;
+    
+    case 66:
+        alert('Вы пенсионер');
+        break;
+    
+    case 67:
+        alert('Вы пенсионер');
+        break;
+    
+    case 68:
+        alert('Вы пенсионер');
+        break;
+    
+    case 69:
+        alert('Вы пенсионер');
+        break;
+    
+    case 70:
+        alert('Вы пенсионер');
+        break;
+    
+    case 71:
+        alert('Вы пенсионер');
+        break;
+    
+    case 72:
+        alert('Вы пенсионер');
+        break;
+    
+    case 73:
+        alert('Вы пенсионер');
+        break;
+    
+    case 74:
+        alert('Вы пенсионер');
+        break;
+    
+    case 75:
+        alert('Вы пенсионер');
+        break;
+    
+    case 76:
+        alert('Вы пенсионер');
+        break;
+    
+    case 77:
+        alert('Вы пенсионер');
+        break;
+    
+    case 78:
+        alert('Вы пенсионер');
+        break;
+    
+    case 79:
+        alert('Вы пенсионер');
+        break;
+    
+    case 80:
+        alert('Вы пенсионер');
+        break;
+    
+    case 81:
+        alert('Вы пенсионер');
+        break;
+    
+    case 82:
+        alert('Вы пенсионер');
+        break;
+    
+    case 83:
+        alert('Вы пенсионер');
+        break;
+    
+    case 84:
+        alert('Вы пенсионер');
+        break;
+    
+    case 85:
+        alert('Вы пенсионер');
+        break;
+    
+    case 86:
+        alert('Вы пенсионер');
+        break;
+    
+    case 87:
+        alert('Вы пенсионер');
+        break;
+    
+    case 88:
+        alert('Вы пенсионер');
+        break;
+    
+    case 89:
+        alert('Вы пенсионер');
+        break;
+    
+    case 90:
+        alert('Вы пенсионер');
+        break;
+    
+    case 91:
+        alert('Вы пенсионер');
+        break;
+    
+    case 92:
+        alert('Вы пенсионер');
+        break;
+    
+    case 93:
+        alert('Вы пенсионер');
+        break;
+    
+    case 94:
+        alert('Вы пенсионер');
+        break;
+    
+    case 95:
+        alert('Вы пенсионер');
+        break;
+    
+    case 96:
+        alert('Вы пенсионер');
+        break;
+    
+    case 97:
+        alert('Вы пенсионер');
+        break;
+    
+    case 98:
+        alert('Вы пенсионер');
+        break;
+    
+    case 99:
+        alert('Вы пенсионер');
+        break;
+    
+    case 100:
+        alert('Вы пенсионер');
+        break;
+    
+    case 101:
+        alert('Вы пенсионер');
+        break;
+    
+    case 102:
+        alert('Вы пенсионер');
+        break;
+    
+    case 103:
+        alert('Вы пенсионер');
+        break;
+    
+    case 104:
+        alert('Вы пенсионер');
+        break;
+    
+    case 105:
+        alert('Вы пенсионер');
+        break;
+    
+    case 106:
+        alert('Вы пенсионер');
+        break;
+    
+    case 107:
+        alert('Вы пенсионер');
+        break;
+    
+    case 108:
+        alert('Вы пенсионер');
+        break;
+    
+    case 109:
+        alert('Вы пенсионер');
+        break;
+    
+    case 110:
+        alert('Вы пенсионер');
+        break;
+    
+    case 111:
+        alert('Вы пенсионер');
+        break;
+    
+    case 112:
+        alert('Вы пенсионер');
+        break;
+    
+    case 113:
+        alert('Вы пенсионер');
+        break;
+    
+    case 114:
+        alert('Вы пенсионер');
+        break;
+    
+    case 115:
+        alert('Вы пенсионер');
+        break;
+    
+    case 116:
+        alert('Вы пенсионер');
+        break;
+    
+    case 117:
+        alert('Вы пенсионер');
+        break;
+    
+    case 118:
+        alert('Вы пенсионер');
+        break;
+    
+    case 119:
+        alert('Вы пенсионер');
+        break;
+    
+    case 120:
+        alert('Вы пенсионер');
+        break;
+    
+    case 121:
+        alert('Вы пенсионер');
+        break;
+    
+    case 122:
+        alert('Вы пенсионер');
+        break;
+    
+    case 123:
+        alert('Вы пенсионер');
+        break;
+    
+    case 124:
+        alert('Вы пенсионер');
+        break;
+    
+    case 125:
+        alert('Вы пенсионер');
+        break;
+    
+    case 126:
+        alert('Вы пенсионер');
+        break;
+    
+    case 127:
+        alert('Вы пенсионер');
+        break;
+    
+    case 128:
+        alert('Вы пенсионер');
+        break;
+    
+    case 129:
+        alert('Вы пенсионер');
+        break;
+    
+    case 130:
+        alert('Вы пенсионер');
+        break;
+    
+    case 131:
+        alert('Вы пенсионер');
+        break;
+    
+    case 132:
+        alert('Вы пенсионер');
+        break;
+    
+    case 133:
+        alert('Вы пенсионер');
+        break;
+    
+    case 134:
+        alert('Вы пенсионер');
+        break;
+    
+    case 135:
+        alert('Вы пенсионер');
+        break;
+    
+    case 136:
+        alert('Вы пенсионер');
+        break;
+    
+    case 137:
+        alert('Вы пенсионер');
+        break;
+    
+    case 138:
+        alert('Вы пенсионер');
+        break;
+    
+    case 139:
+        alert('Вы пенсионер');
+        break;
+    
+    case 140:
+        alert('Вы пенсионер');
+        break;
+    case 141:
+        alert('Вы пенсионер');
+        break;
+    case 142:
+        alert('Вы пенсионер');
+        break;
+    case 143:
+        alert('Вы пенсионер');
+        break;
+    case 144:
+        alert('Вы пенсионер');
+        break;
+    case 145:
+        alert('Вы пенсионер');
+        break;
+    case 146:
+        alert('Вы пенсионер');
+        break;
+    
+    case 147:
+        alert('Вы пенсионер');
+        break;
+    
+    case 148:
+        alert('Вы пенсионер');
+        break;
+    
+    case 149:
+        alert('Вы пенсионер');
+        break;
+    case 150:
+        alert('Вы пенсионер');
+        break;
+    
+    default:
+        alert('Что-то пошло не так');
+        break;
+}
+
+var time = +prompt('введите время');
+
+switch(time){
+    case 1: 
+    alert('час ночи');
+    break;
+    case 2: 
+    alert('2 часа ночи');
+    break;
+    case 3: 
+    alert('3 часа ночи');
+    break;
+    case 4: 
+    alert('4 часа ночи');
+    break;
+    case 5: 
+    alert('5 часов утра');
+    break;
+    case 6: 
+    alert('6 часов утра');
+    break;
+    case 7: 
+    alert('7 часов утра');
+    break;
+    case 8: 
+    alert('8 часов утра');
+    break;
+    case 9: 
+    alert('9 часов утра');
+    break;
+    case 10: 
+    alert('10 часов утра');
+    break;
+    case 11: 
+    alert('11 часов утра');
+    break;
+    case 12: 
+    alert('полдень');
+    break;
+    case 13: 
+    alert('час дня');
+    break;
+    case 14: 
+    alert('2 часа дня');
+    break;
+    case 15: 
+    alert('3 часа дня');
+    break;
+    case 16: 
+    alert('4 часа вечера');
+    break;
+    case 17: 
+    alert('5 часов вечера');
+    break;
+    case 18: 
+    alert('6 часов вечера');
+    break;
+    case 19: 
+    alert('7 часов вечера');
+    break;
+    case 20: 
+    alert('8 часов вечера');
+    break;
+    case 21: 
+    alert('9 часов вечера');
+    break;
+    case 22: 
+    alert('10 часов вечера');
+    break;
+    case 23: 
+    alert('11 часов вечера');
+    break;
+    case 24: 
+    alert('12 часов ночи');
+    break;
+    default:
+        alert('ошибка');
+        break;
+}
